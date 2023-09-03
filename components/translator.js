fb959ae4c43afb84edc94a3c32f65de1e137d0ba
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-09-04 01:43:05 +0500

    Add british to american phrases and spellings functionality

--- components/translator.js
+++ components/translator.js
@@ -4,27 +4,78 @@ import americanToBritishTitles from "./american-to-british-titles.js";
 import britishOnly from "./british-only.js";
 
 class Translator {
-  static adjustCase(inputText) {
+  static #adjustCase(inputText) {
     // match first letter and return it as uppercased
     const substituteTextMatch = new RegExp("^[a-z]", "gi")
     const upperCasedCharFn = (char) => char.toUpperCase()
     const uppercasedText = inputText.replace(substituteTextMatch, upperCasedCharFn)
     return uppercasedText
   }
-  static highlightAndReplace(_ignore, replacement, shouldHighlight=true, needsAdjustment=false) {
+  static #highlightAndReplace(_ignore, replacement, shouldHighlight=false, needsAdjustment=false) {
     if (needsAdjustment) {
       // call upper casing function
-      replacement = Translator.adjustCase(replacement)
+      replacement = Translator.#adjustCase(replacement)
     }
     return () => {
       // replace whitespace with null character
       const newStrWithNullChar = replacement.replace(/\s/g, "\0")
-      shouldHighlight ? `<span class="highlight">${newStrWithNullChar}</span>` :`~${newStrWithNullChar}~`
+      return shouldHighlight ? `<span class="highlight">${newStrWithNullChar}</span>` :`~${newStrWithNullChar}~`
     }
   }
-  static translateAmericanToBritish(input) {
+  static translateAmericanToBritish(input, isHighlighted=false) {
+    // replace phrases
+    for (const [american, british] of Object.entries(americanOnly)) {
+      // catch american word bounded on both sides
+      const americanCapture = new RegExp(`\\b${american}\\b`, "gi")
+      const compressedBritishSubstitute = Translator.#highlightAndReplace(american, british)
+      input = input.replace(americanCapture, compressedBritishSubstitute)
+    }
+
+    // translate spellings
+    for (const [american, british] of Object.entries(americanToBritishSpelling)) {
+      const americanCapture = new RegExp(`\\b${american}\\b`, "gi")
+      const compressedBritishSubstitute = Translator.#highlightAndReplace(american, british)
+      input = input.replace(americanCapture, compressedBritishSubstitute)
+    }
+
+    // translate titles
+    for (const [american, british] of Object.entries(americanToBritishTitles)) {
+      const modifiedAmericanEntry = american.replace('.', '\\.')
+      const americanCapture = new RegExp(`${modifiedAmericanEntry}`, "gi")
+      const compressedBritishSubstitute = Translator.#highlightAndReplace(modifiedAmericanEntry, british, false, true)
+      input = input.replace(americanCapture, compressedBritishSubstitute);
+    }
+
+    // convert colon to dot
+    const colonCapture = new RegExp(/(\d{1,2}):(\d{1,2})/, "gi")
+    isHighlighted ? input = input.replace(colonCapture, `<span class="highlight">$1.$2</span>`)
+      : input = input.replace(colonCapture, `$1.$2`)
+
+    const tildeCapture = new RegExp(/~/, "g")
+    const strWthRemovedTilde = input.replace(tildeCapture, "")
+    const nullCapture = new RegExp(/\0/, "g")
+    const strWithWhitespaces = strWthRemovedTilde.replace(nullCapture, " ");
+    return strWithWhitespaces
   }
-  static translateBritishToAmerican(input) {
+
+  static translateBritishToAmerican(input, isHighlighted=false) {
+    // phrases
+    for (const [british, american] of Object.entries(britishOnly)) {
+      const britishCapture = new RegExp(`\\b${british}\\b`, "gi")
+      const americanSubstitute = Translator.#highlightAndReplace(british, american)
+      input = input.replace(britishCapture, americanSubstitute);
+    }
+    // spellings
+    for (const [american, british] of Object.entries(americanToBritishSpelling)) {
+      const britishCapture = new RegExp(`\\b${british}\\b`, "gi")
+      const americanSubstitute = Translator.#highlightAndReplace(british, american)
+      input = input.replace(britishCapture, americanSubstitute);
+    }
+    const tildeCapture = new RegExp(/~/, "gi")
+    const strWithRemovedTilde = input.replace(tildeCapture, "");
+    const nullCapture = new RegExp(/\0/,"gi")
+    const strWithWhitespaces = strWithRemovedTilde.replace(nullCapture, " ");
+    return strWithWhitespaces
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -11,5 +11,105 @@ suite('Unit Tests', () => {
       assert.strictEqual(translation, output)
       done()
     })
+    test('I ate yogurt for breakfast.', (done) => {
+      const input = 'I ate yogurt for breakfast.'
+      const output = 'I ate yoghurt for breakfast.'
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("We had a party at my friend's condo.", (done) => {
+      const input = "We had a party at my friend's condo."
+      const output = "We had a party at my friend's flat."
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("Can you toss this in the trashcan for me?", (done) => {
+      const input = "Can you toss this in the trashcan for me?"
+      const output = "Can you toss this in the bin for me?"
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("The parking lot was full.", (done) => {
+      const input = "The parking lot was full."
+      const output = "The car park was full."
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("Like a high tech Rube Goldberg machine.", (done) => {
+      const input = "Like a high tech Rube Goldberg machine."
+      const output = "Like a high tech Heath Robinson device."
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("To play hooky means to skip class or work.", (done) => {
+      const input = "To play hooky means to skip class or work."
+      const output = "To bunk off means to skip class or work."
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("No Mr. Bond, I expect you to die.", (done) => {
+      const input = "No Mr. Bond, I expect you to die."
+      const output = "No Mr Bond, I expect you to die."
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("Dr. Grosh will see you now.", (done) => {
+      const input = "Dr. Grosh will see you now."
+      const output = "Dr Grosh will see you now."
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("Lunch is at 12:15 today.", (done) => {
+      const input = "Lunch is at 12:15 today."
+      const output = "Lunch is at 12.15 today."
+      const translation = Translator.translateAmericanToBritish(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+  })
+  suite("British to American English", () => {
+    test('We watched the footie match for a while.', (done) => {
+      const input = "We watched the footie match for a while."
+      const output = "We watched the soccer match for a while."
+      const translation = Translator.translateBritishToAmerican(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("Paracetamol takes up to an hour to work.", (done) => {
+      const input = "Paracetamol takes up to an hour to work."
+      const output = "Tylenol takes up to an hour to work."
+      const translation = Translator.translateBritishToAmerican(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("First, caramelise the onions.", (done) => {
+      const input = "First, caramelise the onions."
+      const output = "First, caramelize the onions."
+      const translation = Translator.translateBritishToAmerican(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("I spent the bank holiday at the funfair.", (done) => {
+      const input = "I spent the bank holiday at the funfair."
+      const output = "I spent the public holiday at the carnival."
+      const translation = Translator.translateBritishToAmerican(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
+    test("I had a bicky then went to the chippy.", (done) => {
+      const input = "I had a bicky then went to the chippy."
+      const output = "I had a cookie then went to the fish-and-chip shop."
+      const translation = Translator.translateBritishToAmerican(input)
+      assert.strictEqual(translation, output)
+      done()
+    })
   })
 })
